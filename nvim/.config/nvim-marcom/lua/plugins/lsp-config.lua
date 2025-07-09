94974391372736e11d0b31b727e9dc7ed2542a64
marcobre <13064322+marcobre@users.noreply.github.com>
2025-07-09 08:55:09 +0200

    added wencam mpv aliases

--- nvim/.config/nvim-marcom/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim-marcom/lua/plugins/lsp-config.lua
@@ -26,7 +26,7 @@ return {
 			lspconfig.lua_ls.setup({
         capabilities = capabilities,
       })
-			lspconfig.tsserver.setup({
+			lspconfig.ts_ls.setup({
 				capabilities = capabilities,
 			})
 			lspconfig.html.setup({
