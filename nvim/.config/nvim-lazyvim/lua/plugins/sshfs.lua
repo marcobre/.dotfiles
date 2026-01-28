30af5945cf3c9065bd2ee6548cf42b93a313579c
marcobre <13064322+marcobre@users.noreply.github.com>
2026-01-28 14:44:23 +0100

    added lazyvim plugins
    added lazygit and sshfs plugins to lazygit config and adjusted noice
    borders and window specs

--- nvim/.config/nvim-lazyvim/lua/plugins/sshfs.lua
+++ nvim/.config/nvim-lazyvim/lua/plugins/sshfs.lua
@@ -46,7 +46,7 @@ return {
       },
     },
     ui = {
-      file_picker = {
+      local_picker = {
         preferred_picker = "auto", -- one of: "auto", "snacks", "fzf-lua", "mini", "telescope", "oil", "neo-tree", "nvim-tree", "yazi", "lf", "nnn", "ranger", "netrw"
         fallback_to_netrw = true, -- fallback to netrw if no picker is available
         netrw_command = "Explore", -- netrw command: "Explore", "Lexplore", "Sexplore", "Vexplore", "Texplore"
